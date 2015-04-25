2b8d367e009fbd3e21ed3e6c73dfbb40b3eb5c17
Jack <792626757@qq.com>
2015-04-25 15:40:48 +0800

    更新js代码至validate-local-ajax.html.js

--- src/js/validate-local-ajax.html.js
+++ src/js/validate-local-ajax.html.js
@@ -1,3 +1,15 @@
 /**
  * Created by Jack on 2015-04-25.
  */
+var samplePson;
+var prehtml = template('person_data',samplePson);
+$("#data").html(prehtml);
+
+$(document).ready(function(){
+  $(document).delegate("#modify", "click", "local-model/person.json", jsonGetFn(show));
+});
+
+function show(jsondata){
+  var prehtml = template('person_data',jsondata);
+  $("#data").html(prehtml);
+}
