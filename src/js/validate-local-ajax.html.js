0c86d5892d786bcec17f63ed039bd159e48ae9f4
Jack <792626757@qq.com>
2015-04-26 15:37:53 +0800

    modify validate-local-ajax.html.js

--- src/js/validate-local-ajax.html.js
+++ src/js/validate-local-ajax.html.js
@@ -3,12 +3,21 @@
  */
 var samplePson = {};
 var prehtml = template('person_data',samplePson);
-$("#data").html(prehtml);
+//$("#data").html(prehtml);
 
 $(document).ready(function(){
   titleChange();
-  $(document).delegate("#modify", "click", "local-model/person.json", jsonGetFn(show));
+  $(document).delegate("#modify", "click", [["local-model/person.json", show], ["local-model/person2.json", show]],toggalRequest());
+  $(document).delegate(".person-cell", "mouseenter", function(){console.log("透明" + this.id);});
 });
+
+function toggalRequest(){
+  var toggal = 1;
+  return function(event) {
+    toggal = (toggal == 0?toggal+1:toggal-1);
+    $.getJSON(event.data[toggal][0], event.data[toggal][1]);
+  }
+}
 
 function show(jsondata){
   var prehtml = template("person_data",jsondata);
